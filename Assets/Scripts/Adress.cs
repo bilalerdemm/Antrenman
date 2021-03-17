da6f51885f6f6c317a043e4228894d5bed078df1
Bilal Erdem <e.bilal@apphicgames.com>
2021-03-17 16:24:00 +0300

    Initial commit

---
+++ Assets/Scripts/Adress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adress : MonoBehaviour
+{
+
+    public GameObject myAdress;
+
+    public GameObject myHome;
+
+    public GameObject myStreet;
+
+    public GameObject A;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAdress = GameObject.Find("Adress");
+
+        
+        myHome = GameObject.Find("/Adress/Turkiye/Istanbul/Beykoz/Sinem Sokak/No 3");
+
+        myStreet = GameObject.FindGameObjectWithTag("Sokak");
+
+        A = GameObject.Find("A/B/C/D");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject myHuman;
+    public GameObject myFinger;
+
+    
+    
+    void Start()
+    {
+        myHuman = GameObject.Find("Human");
+
+        
+        
+        
+        myFinger = GameObject.Find("Human/Body/Legs/Foot Left/Finger 3");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
