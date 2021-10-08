1003beef374884c8c89c8d566afb97f3f825a16c
Bilal Erdem <e.bilal@apphicgames.com>
2021-10-08 10:56:43 +0300

    ss

---
+++ Assets/Carpma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carpma : MonoBehaviour
+{
+    //Bir küp gameobjesi,sphere gameobjectsine çarptığı zaman hem küp gameobjesinin hemde çarptığı sphere gameobjesinin tag’ını “Taha” nasıl yapabiliriz
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            gameObject.tag = "Taha";
+            collision.gameObject.tag = "Taha";
+
+        }
+
+    }
+
+
+
+}
